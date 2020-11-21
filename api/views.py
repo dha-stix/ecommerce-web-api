842cd4bdc13c3afe4fce8698a1155edb8ff4c99c
dha-stix <asaoludavid234@yahoo.com>
2020-11-21 06:22:09 +0100

    jsbsd

--- api/views.py
+++ api/views.py
@@ -6,11 +6,9 @@ from .models import Product
 
 # Create your views here.
 
+
 class ViewAllProducts(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     filter_backends = (SearchFilter,)
     search_fields = ("name",  "category__category")
-
-
-
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny',]}
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ]}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
